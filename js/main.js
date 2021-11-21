0c9d212bdc3574a5968eacec11f6beb83dd9d3f3
bakhtiyar-garashov <bakhtiyar.garashov@ut.ee>
2021-11-21 21:58:15 +0200

    Install Openlayers and connect to WMS

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
-console.log('Hello world');
+import 'ol/ol.css';
+import Map from 'ol/Map';
+import OSM from 'ol/source/OSM';
+import TileLayer from 'ol/layer/Tile';
+import TileWMS from 'ol/source/TileWMS';
+import View from 'ol/View';
 
-console.log('testnew')
+const layers = [
+  new TileLayer({
+    source: new OSM(),
+  }),
+  new TileLayer({
+    source: new TileWMS({
+      url: 'http://34.88.147.186:8080/geoserver/MSc-thesis/wms',
+      params: {'LAYERS': 'MSc-thesis:2c', 'TILED': true},
+      serverType: 'geoserver',
+      // Countries have transparency, so do not fade tiles:
+      transition: 0,
+      projection: 'EPSG:3301'
+    }),
+  }),
+];
+const map = new Map({
+  layers: layers,
+  target: 'map',
+  view: new View({
+    center: [-10997148, 4569099],
+    zoom: 4,
+  }),
+});
